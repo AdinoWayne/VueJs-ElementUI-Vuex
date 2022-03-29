332ff1cd7af2b49bf7ab8203d8a9d89431107df7
Gnurt10 <gnurt10@gmail.com>
2022-03-30 01:15:17 +0700

    [update] D3 component

--- src/views/components/D3.vue
+++ src/views/components/D3.vue
@@ -21,13 +21,43 @@ export default {
   name: 'D3',
   data() {
     return {
-      activeNum: "0"
+      activeNum: "0",
+      svg: null,
+      steps: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+      stepErrors: ['10', '11', '12', '13', '14', '15', '16', '17', '18'],
+      allSteps: [],
+      arrText: [
+            'Scan',
+            'Send to Cloud',
+            'Download from Cloud',
+            'Open SSH',
+            'Send FW',
+            'Send Plume Cas',
+            'Install Plume Cas',
+            'Install FW',
+            'Send Log'
+      ],
+      arrProgress: [],
+      progressBar: null,
+      progressBackground: null,
+      progressError: null,
+      progressFirstBackGround: null,
+      progress: null,
+      arrHighProgress: [],
+      currentState: "success",
+      arrIcon: [],
+      stepWidth: 0,
     };
   },
   computed: {
-    ...mapGetters('post' ,["posts"])
+    ...mapGetters('post' ,["posts"]),
+    colors() {
+        return { green: '#4DC87F', lightGreen: '#D9F0E3', white: '#FFFFFF', black: '#000000' };
+    }
   },
-  created() {},
+  created() {
+      this.allSteps = [...this.steps, ...this.stepErrors];
+  },
   mounted() {
     if (!this.posts || this.posts.length  === 0) {
     //   this.getPosts().then(() => this.initData());
@@ -38,53 +68,111 @@ export default {
     ...mapActions('post', ['getPosts']),
     initData() {
     },
+    updateProgressBar(step_) {
+        var positionI = this.allSteps.indexOf(step_);
+        var positionE = this.stepErrors.indexOf(step_);
+        var steps = this.steps;
+        var stepWidth = this.stepWidth;
+        this.progress.transition()
+            .duration(1000)
+            .attr('fill', this.colors.green)
+            .attr('width', () => {
+                var index = this.allSteps.indexOf(step_);
+                if (index < steps.length) {
+                    return (index) * stepWidth;
+                }
+                return (index - steps.length + 1) * stepWidth;
+            });
+        if (positionI >= steps.length) {
+            this.progressFirstBackGround
+            .attr('height', () => {
+                return 128;
+            });
+            this.progressError
+            .attr('width', () => {
+                var index = this.allSteps.indexOf(step_);
+                return (index - steps.length + 1) * stepWidth;
+            });
+            this.arrProgress[positionE]
+            .attr('height', () => {
+                return 128;
+            });
+            d3.select('#step_' + positionI)
+            .attr('fill', this.colors.green)
+            .attr('stroke', this.colors.green)
+            .style("display", "block");
+            this.arrHighProgress[positionE + 1].transition()
+            .delay(500)
+            .duration(1000)
+            .attr('fill', this.colors.green)
+            .attr('height', 128);
+        }
+        for(let i = 0; i < this.allSteps.length; i++){
+            if(i <= positionI && positionI < steps.length) {
+                d3.select('#step_' + i).attr('fill', this.colors.green).attr('stroke', this.colors.green);
+                if (this.currentState == 'pending' && this.allSteps[i] == this.activeNum) {
+                    d3.select('#foreign_' + i).html('<i class="el-icon-loading"></i>');
+                } else if (i == 0) {
+                    d3.select('#foreign_' + i).html('<i class="el-icon-refresh"></i>');
+                } else {
+                    if (this.currentState == 'failed') {
+                        d3.select('#foreign_' + i).html('<i class="el-icon-close"></i>');
+                    } else {
+                        d3.select('#foreign_' + i).html('<i class="el-icon-check"></i>');
+                    }
+                }
+            } else if (i <= positionI - steps.length + 1 || i == positionI) {
+                d3.select('#step_' + i).attr('fill', this.colors.green).attr('stroke', this.colors.green);
+                if (i == 0) {
+                    d3.select('#foreign_' + i).html('<i class="el-icon-refresh"></i>').style("display", "block");
+                } else {
+                    d3.select('#foreign_' + i).html('<i class="el-icon-check"></i>').style("display", "block");
+                }
+                if (i == positionI) {
+                    d3.select('#label_' + i).attr('fill', this.colors.black).style("display", "block");
+                }
+            } else {
+                if (i >= steps.length) {
+                    d3.select('#label_' + i).attr('fill', this.colors.black).style("display", "none");
+                }
+                d3.select('#step_' + i).attr('fill', this.colors.lightGreen).attr('stroke', this.colors.lightGreen);
+            }
+        }
+    },
     initStep() {
-        var colors = { green: '#4DC87F', lightGreen: '#D9F0E3', white: '#FFFFFF', black: '#000000' };
         var width = 960, height = 480, offset = 48;
 
         width += offset * 2;
         height += offset * 2;
         var dimensions = '' + 0 + ' ' + 0 + ' ' + width + ' ' + height;
 
-        var svg = d3.select('.wrapper-ap-progress').append('svg')
+        this.svg = d3.select('.wrapper-ap-progress').append('svg')
             .attr('id', 'scene', true)
             .attr('preserveAspectRatio', 'xMinYMin meet')
             .attr('viewBox', dimensions)
             .classed('svg-content', true);
 
-        var steps = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-        var stepErrors = ['10', '11', '12', '13', '14', '15', '16', '17', '18'];
+        var steps = this.steps;
+        var stepErrors = this.stepErrors;
 
-        var arrText = [
-            'Scan',
-            'Send to Cloud',
-            'Download from Cloud',
-            'Open SSH',
-            'Send FW',
-            'Send Plume Cas',
-            'Install Plume Cas',
-            'Install FW',
-            'Send Log'
-        ];
-        var arrProgress = [];
-        var arrHighProgress = [];
-        var arrIcon = [];
-        var stepWidth = Math.floor((width - offset * 2) / (steps.length - 1)),
-        currentStep = '0';
+        var arrText = this.arrText;
+        this.stepWidth = Math.floor((width - offset * 2) / (steps.length - 1));
+        var stepWidth = this.stepWidth;
+        var currentStep = '0';
 
-        var progressBar = svg.append('g')
+        this.progressBar = this.svg.append('g')
                     .attr('transform', 'translate(' + offset + ',' + offset + ')');
 
         // eslint-disable-next-line no-unused-vars
-        var progressBackground = progressBar.append('rect')
-            .attr('fill', colors.lightGreen)
+        this.progressBackground = this.progressBar.append('rect')
+            .attr('fill', this.colors.lightGreen)
             .attr('height', 8)
             .attr('width', width - offset * 2)
             .attr('rx', 4)
             .attr('ry', 4);
 
-        var progressError = progressBar.append('rect')
-            .attr('fill', colors.lightGreen)
+        this.progressError = this.progressBar.append('rect')
+            .attr('fill', this.colors.lightGreen)
             .attr('height', 8)
             .attr('width', 0)
             .attr('rx', 4)
@@ -92,8 +180,8 @@ export default {
             .attr("x", 0)
             .attr("y", 120);
 
-        var progressFirstBackGround = progressBar.append('rect')
-            .attr('fill', colors.lightGreen)
+        this.progressFirstBackGround = this.progressBar.append('rect')
+            .attr('fill', this.colors.lightGreen)
             .attr('height', 0)
             .attr('width', 8)
             .attr('rx', 4)
@@ -101,9 +189,9 @@ export default {
             .attr("x", -4)
             .attr("y", -1)
 
-        for (var index = 0; index < stepErrors.length; index++) {
-            arrProgress.push(progressBar.append('rect')
-            .attr('fill', colors.lightGreen)
+        for (let index = 0; index < stepErrors.length; index++) {
+            this.arrProgress.push(this.progressBar.append('rect')
+            .attr('fill', this.colors.lightGreen)
             .attr('height', 0)
             .attr('width', 8)
             .attr('id', "vertical_" + stepErrors[index])
@@ -113,16 +201,16 @@ export default {
             .attr("y", -1));
         }
 
-        var progress = progressBar.append('rect')
-            .attr('fill', colors.green)
+        this.progress = this.progressBar.append('rect')
+            .attr('fill', this.colors.green)
             .attr('height', 8)
             .attr('width', 0)
             .attr('rx', 4)
             .attr('ry', 4);
 
         for (let index = 0; index < stepErrors.length; index++) {
-            arrHighProgress.push(progressBar.append('rect')
-            .attr('fill', colors.green)
+            this.arrHighProgress.push(this.progressBar.append('rect')
+            .attr('fill', this.colors.green)
             .attr('height', 0)
             .attr('width', 8)
             .attr('rx', 4)
@@ -131,15 +219,15 @@ export default {
             .attr("y", -1))
         }
 
-        progress.transition()
+        this.progress.transition()
             .duration(1000)
-            .attr('width', function(){
-                var index = [...steps, ...stepErrors].indexOf(currentStep);
+            .attr('width', () => {
+                var index = this.allSteps.indexOf(currentStep);
                 return (index + 1) * stepWidth;
             });
 
-        progressBar.selectAll('circle')
-        .data([...steps, ...stepErrors])
+        this.progressBar.selectAll('circle')
+        .data(this.allSteps)
         .enter()
         .append('circle')
         .attr('id', function(d, i){ return 'step_' + i; })
@@ -156,8 +244,8 @@ export default {
             return 4; 
         })
         .attr('r', 10)
-        .attr('fill', colors.white)
-        .attr('stroke', colors.lightGreen)
+        .attr('fill', this.colors.white)
+        .attr('stroke', this.colors.lightGreen)
         .attr('stroke-width', 6)
         .style("cursor", "pointer")
         .style("display", function(d) {
@@ -167,12 +255,12 @@ export default {
             return "block";
         });
 
-        for (let index = 0; index < [...steps, ...stepErrors].length; index++) {
+        for (let index = 0; index < this.allSteps.length; index++) {
             let html = '<i class="el-icon-arrow-right"></i>'
             if (index == 0) {
-                html = '<i class="el-icon-check"></i>';
+                html = '<i class="el-icon-refresh"></i>';
             }
-            arrIcon.push( progressBar.append('svg:foreignObject')
+            this.arrIcon.push(this.progressBar.append('svg:foreignObject')
             .attr('id', 'foreign_' + index)
             .attr('height', '20px')
             .attr('x', function() {
@@ -185,7 +273,7 @@ export default {
                 if (index >= steps.length) {
                     return 112; 
                 }
-                return -7
+                return -8
             })
             .attr('width', '20px')
             .style("cursor", "pointer")
@@ -196,13 +284,19 @@ export default {
                 return "block";
             })
             .html(html)
-            .on('click', function() {
-                updateProgressBar((index).toString());
+            .on('click', () => {
+                this.activeNum = index.toString();
+                this.currentState = 'pending';
+                this.updateProgressBar(this.activeNum);
+                setTimeout(() => {
+                    this.currentState = 'failed';
+                    this.updateProgressBar(this.activeNum);
+                }, 3000)
             }))
         }
 
-        progressBar.selectAll('text')
-        .data([...steps, ...stepErrors])
+        this.progressBar.selectAll('text')
+        .data(this.allSteps)
         .enter()
         .append('text')
         .attr('id', function(d, i){ return 'label_' + i; })
@@ -214,7 +308,7 @@ export default {
         })
         .attr('dy', function(d) {
             if (d >= steps.length) {
-                return 130; 
+                return 150; 
             }
             return -25; 
         })
@@ -230,54 +324,7 @@ export default {
         })
         .style("font-size", '9px')
 
-        updateProgressBar(this.activeNum);
-
-        function updateProgressBar(step_){
-            var positionI = [...steps, ...stepErrors].indexOf(step_);
-            var positionE = stepErrors.indexOf(step_);
-            progress.transition()
-                .duration(1000)
-                .attr('fill', colors.green)
-                .attr('width', function(){
-                    var index = [...steps, ...stepErrors].indexOf(step_);
-                    if (index < steps.length) {
-                        return (index) * stepWidth;
-                    }
-                    return (index - steps.length + 1) * stepWidth;
-                });
-            if (positionI >= steps.length) {
-                progressFirstBackGround
-                .attr('height', function(){
-                    return 128;
-                });
-                progressError
-                .attr('width', function(){
-                    var index = [...steps, ...stepErrors].indexOf(step_);
-                    return (index - steps.length + 1) * stepWidth;
-                });
-                arrProgress[positionE]
-                .attr('height', function(){
-                    return 128;
-                });
-                d3.select('#step_' + positionI).attr('fill', colors.green).attr('stroke', colors.green).style("display", "block");
-                arrHighProgress[positionE + 1].transition()
-                .delay(500)
-                .duration(1000)
-                .attr('fill', colors.green)
-                .attr('height', 128);
-            }
-            for(var i = 0; i < [...steps, ...stepErrors].length; i++){
-                if(i <= positionI && positionI < steps.length) {
-                    d3.select('#step_' + i).attr('fill', colors.green).attr('stroke', colors.green);
-                    d3.select('#foreign_' + i).html('<i class="el-icon-check"></i>');
-                } else if (i <= positionI - steps.length + 1 || i == positionI) {
-                    d3.select('#step_' + i).attr('fill', colors.green).attr('stroke', colors.green);
-                } else {
-                    d3.select('#step_' + i).attr('fill', colors.lightGreen).attr('stroke', colors.lightGreen);
-                    d3.select('#label_' + i).attr('fill', colors.black);
-                }
-            }
-        }
+        this.updateProgressBar(this.activeNum);
     }
   }
 };
