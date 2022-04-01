e53d498e6341fd83b3f9679a60c03905048f325b
tqtrung <tqtrung@humaxdigital.com>
2022-04-01 18:01:53 +0700

    update merge 2

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -4,7 +4,7 @@ export default function authHeader() {
   let user = getLocalStorage('user');
 
   if (user) {
-    return { Authorization: user }; // for Spring Boot back-end
+    return { Authorization: user.token }; // for Spring Boot back-end
     // return { 'x-access-token': user.accessToken };       // for Node.js Express back-end
   } else {
     return {};
