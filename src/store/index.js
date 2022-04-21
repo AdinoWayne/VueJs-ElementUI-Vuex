9537650c534e47d3f31e585ba67e32be3c143e5e
tqtrung <tqtrung@humaxdigital.com>
2022-04-21 14:36:21 +0700

    remove class useless

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import { auth } from './auth.module';
-import { post } from './post.module';
 import { pi } from './pi.module';
 
 Vue.use(Vuex);
@@ -10,7 +9,6 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   modules: {
     auth,
-    post,
     pi
   }
 });
